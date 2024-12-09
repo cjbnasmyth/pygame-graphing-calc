7e61976642b9835f1a2025c4ca7b78a37ee75bcb
cjbnasmyth <cjbnasmyth@gmail.com>
2024-12-09 14:14:48 +0000

    .

--- functions.py
+++ functions.py
@@ -1,55 +1,171 @@
 import pygame
+import pygame.display
 from pygame.locals import *
 import pygame_gui
-import pygame_gui.elements.ui_horizontal_scroll_bar
+import pygame_widgets as pw
+from pygame_widgets.textbox import TextBox
+from pygame_widgets.button import Button
+import numpy as np
+import math
 
 
-class Functions:
+# WORK OUT CLASS STRUCTURE
+class Interfaces:
     def __init__(self, m, w, s):
         self.window = w
         self.manager = m
         self.surface = s
-    # def update(self):
-    #     return pygame.display.update()
+        self.widgets = {}
+        self.state = 'start_menu'
+        self.menu_created = False
+        self.hMove, self.vMove = 0, 0
+
+    def hideWidgets(self):
+        for i in self.widgets.values():
+            i.hide()
+
+    def renderWidgets(self):
+        for i in self.widgets.values():
+            i.render()
+
+    def getCameraPos(self):
+        return [self.hMove, self.vMove]
+
+    def changeState(self):
+        if self.state =='start_menu':
+            self.menu_created = False
+            self.hideWidgets()
+            self.state = 'main_menu'
+        else:
+            pass
+        print("state changed")
 
 
-class graphContainer(Functions):
-    # def __init__(self,c):
-    #     self.container = c
+# KEEP CODING THIS
+class startMenu(Interfaces):
+    def createStartMenu(self):
+        self.menu_created = True
+        self.window.fill((0,0,0))
+        self.widgets['username'] = TextBox(
+            self.window, 300, 100,
+            200,80,
+            placeholderText = 'Enter Username'
+        )
+        # ADD PASSWORD FONT
+        self.widgets['password'] = TextBox(
+            self.window, 300, 200,
+            200, 80,
+            placeholderText = 'Enter Password'
+        )
+        self.widgets['teacher'] = Button(
+            self.window, 300, 300,
+            100, 80,
+            text = 'Teacher'
+        )
+        self.widgets['student'] = Button(
+            self.window, 400, 300,
+            100, 80,
+            text = 'Student'
+        )
+        self.widgets['log_in_submit'] = Button(
+            self.window, 300, 400,
+            200, 80,
+            text = 'Submit',
+            onClick = self.changeState
+        )
 
-    # def createContainer(self):
 
 
-    # def createScrollingContainer(self):
-    #     # self.s = pygame_gui.elements.UIScrollingContainer(relative_rect=pygame.Rect((0,0),(800,600)), manager=self.manager, allow_scroll_x= True, allow_scroll_y= True, object_id= 'graph')
-    #     # self.s.set_scrollable_area_dimensions((2000,2000)) #Must be greater than 800,600
-    #     self.hBar = pygame_gui.elements.UIHorizontalSlider(relative_rect=pygame.Rect((0,580),(780,20)), start_value= 0, value_range=[0,380], manager=self.manager)
-    #     self.vBar = pygame_gui.elements.UIHorizontalSlider(relative_rect=pygame.Rect((780,0),(20,580)), start_value=0, value_range=[0,380], manager=self.manager)
-    #     # self.hBar.set_current_value(195,False)
-    #     return self.hBar, self.vBar
 
-    def graphMovement(self):
+class mainMenu(Interfaces):
+    def createMainMenu(self):
+        self.menu_created = True
+        self.window.fill((255,255,255))
+        graph_surface = pygame.Surface((600,600))
+        graph_surface.fill((255,255,255))
+        self.widgets['inputEntry1'] = TextBox(
+            self.window, 600, 200, 200, 80
+        )
         
-    def draw_axis(self):
+        self.drawAxis(graph_surface)
+
+        # self.renderWidgets
+        self.window.blit(graph_surface, (0,0))
+
+    def drawAxis(self, s):
+        self.drawXaxis(s)
+        self.drawYAxis(s)
+        self.drawZAxis(s)
+
+    def drawXaxis(self, surface):
         # x line
-        pygame.draw.line(self.window, (0,0,0),
+        xStart = self.trigCalc([-300, 0,  0])
+        xEnd = self.trigCalc([300,0,0])
+        print([xStart, xEnd])
+        pygame.draw.line(surface, (255,0,0),
                         [0, 300],
-                        [780,300], 2)
-        
-        # y line
-        pygame.draw.line(self.window, (0,0,0),
-                        [400,580],
-                        [400,0], 2)
+                        [600,300], 2)
     
-    def redraw(self):
-        pass
+    def drawYAxis(self, surface):
+        # y line
+        yStart = self.trigCalc([0,-300,0])
+        yEnd = self.trigCalc([0,300,0])
+        print([yStart, yEnd])
+        pygame.draw.line(surface, (0,255,0),
+                        yEnd,
+                        yStart, 2)
+    
+    def drawZAxis(self, surface):
+        # z line
+        zStart = self.trigCalc([0,0,-300])
+        zEnd = self.trigCalc([0,0,300])
+        print([zStart, zEnd])
+        pygame.draw.line(surface, (0,0,255),
+                         zEnd,
+                         zStart, 2)
 
-    def trigCalc(self):
-        pass
+    def redrawPoints(self):
+        # change 'a' to input!!!!!!
+        newPos = self.trigCalc(self.handleInput('a'))
+        # DONE IN trigCalc? to ge thhe points to the center of the section ie 300, 300
+        # newPos[0] += 300
+        # newPos[1] += 300
+        pygame.draw.circle(self.window, (0,0,0), (newPos[0],newPos[1]), 4)
 
-    def getScollValue(self):
-        pass
 
-class hudUI(Functions):
-    def createGUI(self):
-        self.hud = pygame_gui.elements.UIPanel(relative_rect=pygame.Rect((800,600) ,(800,600)), manager=self.manager, object_id='hud')
+    def trigCalc(self, elementPosition):
+        # x,y,z is supposed to be the 'orientation of the camera'
+        # Below is camera orientation
+        cameraPostion = self.getCameraPos()
+        # for i in range(0, len(elementPosition)):
+        #     if elementPosition[i] is not 0:
+        #         if i == 0:
+        #             thetaX = self.hMove/math.sqrt((600^2)+(self.hMove^2))
+        #             thetaX = math.cos(thetaX)
+        #         elif i == 1:
+        #             theta
+        thetaX, thetaY, thetaZ = 0, 0, 0
+        # A array is the 3D position of the point being projetced (ie the point before projection)
+        aArray = np.array([elementPosition[0],elementPosition[1],elementPosition[2]])
+        # what value for z??????
+        cArray = np.array([cameraPostion[0],cameraPostion[1],1])
+        subPostion = np.subtract(aArray, cArray)
+        print(subPostion)
+        # in matrix form????
+        # WHAT SCALE????????? - normal coord scale which should thne be adjusted like line 114, 115
+        dX = math.cos(thetaY)*(math.sin(thetaZ)*subPostion[1]+math.cos(thetaZ)*subPostion[0]) - math.sin(thetaY)*subPostion[2]
+        dY = math.sin(thetaX)*(math.cos(thetaY)*subPostion[2]+math.sin(thetaY)*(math.sin(thetaZ)*subPostion[1]+math.cos(thetaZ)*subPostion[0])+math.cos(thetaX)*(math.cos(thetaZ*subPostion[1]-math.sin(thetaZ)*subPostion[0])))
+        dZ = math.cos(thetaX)*(math.cos(thetaY)*subPostion[2]+math.sin(thetaY)*(math.sin(thetaZ)*subPostion[1]+math.cos(thetaZ)*subPostion[0])-math.sin(thetaX)*(math.cos(thetaZ*subPostion[1]-math.sin(thetaZ)*subPostion[0])))    
+        print(f'dZ = {dZ}')
+        if dZ == 0:
+            print('Div by 0 error')
+            return None, None
+        projectedX = (cArray[2]/dZ)*dX + cArray[0]
+        projetcedY = (cArray[2]/dZ)*dY + cArray[1]
+        return [projectedX+300, projetcedY+300]
+
+    # for working out the 3d coords of each point after input (whether that is equation or whatever)
+    def handleInput(self, equation):
+        return [1,1,1]      
+
+
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import pygame_gui.ui_manager
+import pygame_widgets
 from functions import *
 
 pygame.init()
@@ -10,23 +11,42 @@ manager= pygame_gui.UIManager((width, length))
 window = pygame.display.set_mode((width, length))
 window.fill((255,255,255))
 
-bottomLayer = graphContainer(manager, window, surface)
-hBar = bottomLayer.createScrollingContainer()
+hMove, vMove = 0, 0
+cameraArray = [hMove,vMove]
+bottomLayer = mainMenu(manager, window, surface)
+topLayer = startMenu(manager,window, surface)
+# hBar = bottomLayer.createScrollingContainer()
 # topLayer = hudUI(manager, window, surface)
 # for scrolling to work there must be something in the graph container
-bottomLayer.draw_axis()
-pygame.display.update() #embedded into a Functions class
+#embedded into a Functions class
+
+topLayer.createStartMenu()
 
 clock = pygame.time.Clock()
 running = True
 while running:
     time_delta = clock.tick(60)/1000
-    for event in pygame.event.get():
+    events = pygame.event.get()
+    for event in events:
         if event.type == pygame.QUIT:
             running = False
-        
-        manager.process_events(event)
-    # print(hBar.get_current_value())
+        if event.type == pygame.KEYDOWN and bottomLayer.menu_created == True:
+            if event.key == K_LEFT:
+                bottomLayer.hMove -= 1
+            if event.key == K_RIGHT:
+                bottomLayer.hMove += 1
+            if event.key == K_UP:
+                bottomLayer.vMove += 1
+            if event.key == K_DOWN:
+                bottomLayer.vMove -= 1
+            print('redrawn')
+            window.fill((255,255,255))
+            bottomLayer.redrawPoints()
+        if topLayer.state == 'main_menu':
+            bottomLayer.createMainMenu()
+    manager.process_events(event)
+
     manager.update(time_delta)
     manager.draw_ui(window)
+    pygame_widgets.update(events)
     pygame.display.update()
